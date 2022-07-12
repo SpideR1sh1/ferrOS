d8fea752c0513cc4c257af2d5f4b3af04bc14b6b
SpideR1sh1 <86785890+SpideR1sh1@users.noreply.github.com>
2022-07-12 22:51:04 +0530

    Created Rust binary

---
+++ definitions.md
@@ -0,0 +1,7 @@
+All definitions learnt
+
+Minimal Rust Kernel
+
+Stack unwinding: When an exception is thrown and control passes from a try block to a handler, the
+C++ runtime calls destructors for all automatic objects constructed in the try block
+    
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+static HELLO: &[u8] = b"Hello World!";
 
 use core::panic::PanicInfo;
 
@@ -7,15 +8,27 @@ use core::panic::PanicInfo;
 // *Disables name mangling i.e technique used to solve probs caused
 // *by name clashes in the code.
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    panic!("Hello World!");
-}
+
 
 // *basically like an exception handler but without std
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    loop {}
+    // ! the entry point as linker is looking for _start func by default
+
 }
 
 // ! run with this [cargo build --target thumbv7em-none-eabihf]
