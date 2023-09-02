1c6d25e0c9a84d58cf560aab1d31037b509ac8de
Harmya Bhatt <harmyacs@gmail.com>
2023-09-01 22:48:10 -0400

    Update main.rs
    Updating ferrOS.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,7 @@
-#![no_std]
-#![no_main]
-static HELLO: &[u8] = b"Hello World!";
+//Hi Rishi, I think this would be a better way to boot up the OS in Rust. Let me know what you think.
 
-use core::panic::PanicInfo;
-
-
-// *Disables name mangling i.e technique used to solve probs caused
-// *by name clashes in the code.
-
-
-
-// *basically like an exception handler but without std
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn main() {
+    loop { //loop
+        println!("Hello, Mom!"); //print statement
+    } //loop ends
 }
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-    loop {}
-    // ! the entry point as linker is looking for _start func by default
-
-}
-
-// ! run with this [cargo build --target thumbv7em-none-eabihf]
-
-
-
